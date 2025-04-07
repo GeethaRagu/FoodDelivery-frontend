a30a4312a432ba61df49c2c75fd580b81d528a08
GeethaRagu <geethumsp@gmail.com>
2025-04-07 11:40:31 +0530

    updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -90,7 +90,7 @@ const Header = ({ setSignIn }) => {
 
               <li>
                 <Link
-                  onClick={() => setMenu("contact-us")}
+                  onClick={() => setMenu("contact")}
                   to="/contact"
                   className={`block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent ${
                     menu === "contact-us" ? "active" : ""
